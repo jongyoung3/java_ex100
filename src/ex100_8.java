1d5bea88d686d8f157edabf6a95d598ac7beadee
jongyoung3 <jongyong5645@gmail.com>
2023-10-17 23:24:46 +0900

    7, 8번 문제 추가

---
+++ src/ex100_8.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ex100_8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String i = scan.nextLine();
+        System.out.print(i);
+    }
+}
