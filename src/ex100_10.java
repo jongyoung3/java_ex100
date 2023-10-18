d59bd0ebeb3f0b243eadc475e3a6ab598932706d
jongyoung3 <jongyong5645@gmail.com>
2023-10-18 10:34:27 +0900

    9, 10번 문제 추가

---
+++ src/ex100_10.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ex100_10 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i1 = scan.nextInt();
+        int i2 = scan.nextInt();
+        System.out.print(i1);
+        System.out.print(" "+i2);
+    }
+}
---
+++ src/ex100_9.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ex100_9 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Float i = scan.nextFloat();
+        System.out.print(i);
+    }
+}
