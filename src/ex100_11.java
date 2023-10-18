859d18bc14fbe62386602eede771828c5d048900
jongyoung3 <jongyong5645@gmail.com>
2023-10-18 22:17:00 +0900

    11번 문제 추가

---
+++ src/ex100_11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ex100_11 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String i1 = scan.nextLine();
+        String i2 = scan.nextLine();
+        System.out.print(i2);
+        System.out.print(" "+i1);
+    }
+}
